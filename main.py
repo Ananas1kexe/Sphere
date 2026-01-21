813a12d8550ab990dea5a2f251d48f3ac9aaf9fb
Kiril <csgocvadimkiril80@gmail.com>
2026-01-21 17:20:13 +0100

    basic working version

---
+++ main.py
@@ -0,0 +1,19 @@
+import typer
+
+from typing import List, Optional
+from pathlib import Path
+
+import app_version
+
+app = typer.Typer(help="Hello")
+
+@app.command()
+def main(show_version: bool = typer.Option(False, '-v', '--version')):
+    if show_version:
+        typer.echo(app_version.VERSION)
+        raise typer.Exit()
+
+
+
+if __name__ == "__main__":
+    app()
