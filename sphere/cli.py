0f21069dc8bd3e7814643d851fb5d15084568f48
Kiril <csgocvadimkiril80@gmail.com>
2026-01-21 23:07:48 +0100

    struktura projekt

--- sphere/cli.py
+++ sphere/cli.py
@@ -10,16 +10,22 @@ import  sphere_native_rust
 app = typer.Typer(help="Sphere {version} Command Line Interface".format(version=app_version.VERSION))
 
 @app.command()
-def main(show_version: bool = typer.Option(False, '-v', '--version', help="Show application version"),
-         show_cpu: bool = typer.Option(False, '-cpui', '--cpu_info', help="Show cpu version")):
+def main(
+    show_version: bool = typer.Option(False, '-v', '--version'),
+    show_cpu: bool = typer.Option(False, '--cpu'),
+    show_ram: bool = typer.Option(False, '--ram'),
+    optimize: bool = typer.Option(False, '--optimize')
+):
     if show_version:
         typer.echo(app_version.VERSION)
-        raise typer.Exit()
     if show_cpu:
-        cpu_count = sphere_native_rust.cpu_count()
-        typer.echo(f"CPU Count: {cpu_count}")
-        raise typer.Exit()
-
+        typer.echo(f"CPU Count: {cpu.get_cpu_count()}")
+    if show_ram:
+        typer.echo(f"RAM Info: {ram.get_ram_info()}")
+    if optimize:
+        tasks.run_optimization()
+    if not (show_version or show_cpu or show_ram or optimize):
+        typer.echo("Use --help to see available options.")
 
 if __name__ == "__main__":
     app()
