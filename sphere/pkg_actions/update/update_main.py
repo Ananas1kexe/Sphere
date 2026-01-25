498c65b7188f36d99b6a88417dcaf7eab5f2530c
Kiril <csgocvadimkiril80@gmail.com>
2026-01-25 23:07:29 +0100

    update: add system update command logic

--- sphere/pkg_actions/update/update_main.py
+++ sphere/pkg_actions/update/update_main.py
@@ -1,9 +1,31 @@
 import subprocess
 import typer
 
-def update_main():
+from typing import Optional
+from dataclasses import dataclass
+
+from ..get_package_manager import get_package_manager
+from .flag_manager import flag_update_manager
+@dataclass
+class Update:
+    sudo: str = "sudo"
+    pkg: Optional[str] = None
+    update: str = "update"
+
+
+def update_main(
+        flag1: bool,
+        flag2: bool,
+):
+
+    u = Update(pkg=get_package_manager())
+
+    list = flag_update_manager(u.pkg,flag1,flag2)
+
+    arguments = [u.sudo, u.pkg, u.update] + list
+
     result = subprocess.run(
-        ["sudo", "dnf", "update"],
+        arguments,
         stdin=None,
         stdout=None,
         stderr=None
