286f71539fd2f5481ac3f6778fc5fdbfabaf2890
Kiril <csgocvadimkiril80@gmail.com>
2026-01-27 21:14:07 +0100

    edit users

--- sphere/hardware/users/commands_users.py
+++ sphere/hardware/users/commands_users.py
@@ -4,7 +4,7 @@ import typer
 
 from .show_user import show_user
 
-def commands_users_update(app: typer.Typer):
+def commands_users(app: typer.Typer):
 
     @app.command("users", help="Show all user")
     def user_show():
