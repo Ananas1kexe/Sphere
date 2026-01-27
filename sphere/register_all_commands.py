ea5c38d374acaa3c0ad50485dce683f8925c2337
Kiril <csgocvadimkiril80@gmail.com>
2026-01-27 21:56:16 +0100

    update

--- sphere/register_all_commands.py
+++ sphere/register_all_commands.py
@@ -2,7 +2,7 @@
 from .pkg_actions.register_pkg_command import register_pkg_commands
 from .hardware.register_hardware_command import register_hardware_commands
 from .build.register_build_command import register_build_commands
-
+from .other.register_other_command import register_other_commands
 
 def register_all_commands(app):
 
@@ -11,3 +11,5 @@ def register_all_commands(app):
     register_hardware_commands(app)
     
     register_build_commands(app)
+
+    register_other_commands(app)
